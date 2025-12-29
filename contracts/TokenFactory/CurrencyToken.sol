6fe23f4e95c2732216be554cff1d0352df90d113
Ashitosh <ashitosh.bhade@baerscrest.com>
2025-12-29 18:42:38 +0530

    add batch transfer

--- contracts/TokenFactory/CurrencyToken.sol
+++ contracts/TokenFactory/CurrencyToken.sol
@@ -40,16 +40,94 @@ contract CurrencyToken is ERC20Permit, Ownable {
     }
 
     /// ********************************** Functions ****************************************
-
     function transfer(
         address to,
         uint256 amount,
         string memory _reference
     ) public virtual {
-        if (transfer(to, amount)) {
+        if (super.transfer(to, amount)) {
             emit TransferSuccess(_msgSender(), to, amount, _reference);
         } else {
             revert TransferFailed(_msgSender(), to, amount, _reference);
+        }
+    }
+
+    function batchTransfer(
+        address[] calldata to,
+        uint256[] calldata amounts,
+        string[] calldata references
+    ) external {
+        uint256 len = to.length;
+
+        require(
+            len == amounts.length && len == references.length,
+            "Length mismatch"
+        );
+
+        address sender = _msgSender();
+
+        for (uint256 i = 0; i < len; ) {
+            bool success = super.transfer(to[i], amounts[i]);
+
+            if (success) {
+                emit TransferSuccess(sender, to[i], amounts[i], references[i]);
+            } else {
+                revert TransferFailed(sender, to[i], amounts[i], references[i]);
+            }
+
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount,
+        string memory _reference
+    ) public virtual {
+        bool success = super.transferFrom(from, to, amount);
+
+        if (success) {
+            emit TransferSuccess(from, to, amount, _reference);
+        } else {
+            revert TransferFailed(from, to, amount, _reference);
+        }
+    }
+
+    function batchTransferFrom(
+        address[] calldata from,
+        address[] calldata to,
+        uint256[] calldata amounts,
+        string[] calldata references
+    ) external {
+        uint256 len = to.length;
+
+        require(
+            len == amounts.length &&
+                len == references.length &&
+                len == from.length,
+            "Length mismatch"
+        );
+
+        for (uint256 i = 0; i < len; ) {
+            bool success = super.transferFrom(from[i], to[i], amounts[i]);
+
+            if (success) {
+                emit TransferSuccess(from[i], to[i], amounts[i], references[i]);
+            } else {
+                revert TransferFailed(
+                    from[i],
+                    to[i],
+                    amounts[i],
+                    references[i]
+                );
+            }
+
+            unchecked {
+                ++i;
+            }
         }
     }
 
